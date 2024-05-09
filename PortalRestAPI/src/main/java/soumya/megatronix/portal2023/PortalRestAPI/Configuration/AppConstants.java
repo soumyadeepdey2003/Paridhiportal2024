cbf3bd4382e21f1c1aa972609063b580a82f0d49
soumyadeep dey <105117395+soumyadeepdey2003@users.noreply.github.com>
2024-05-09 18:50:12 +0530

    final update

--- PortalRestAPI/src/main/java/soumya/megatronix/portal2023/PortalRestAPI/Configuration/AppConstants.java
+++ PortalRestAPI/src/main/java/soumya/megatronix/portal2023/PortalRestAPI/Configuration/AppConstants.java
@@ -1,5 +1,5 @@
 package soumya.megatronix.portal2023.PortalRestAPI.Configuration;
 
 public class AppConstants {
-    public static final long JWT_TOKEN_VALIDITY = 15 * 24 * 60 * 60;
+    public static final long JWT_TOKEN_VALIDITY = 3*60*60*100;
 }
