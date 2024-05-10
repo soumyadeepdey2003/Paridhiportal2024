3eb092e475a1d4a6d024f0904d1b66ea63f46224
soumyadeep dey <105117395+soumyadeepdey2003@users.noreply.github.com>
2024-05-10 19:09:08 +0530

    Update PortalRestApiApplication.java

--- PortalRestAPI/src/main/java/soumya/megatronix/portal2023/PortalRestAPI/PortalRestApiApplication.java
+++ PortalRestAPI/src/main/java/soumya/megatronix/portal2023/PortalRestAPI/PortalRestApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 //@CrossOrigin(origins = "https://msitparidhi.in")
-@CrossOrigin(origins = "**")
 @SpringBootApplication
 public class PortalRestApiApplication {
 
